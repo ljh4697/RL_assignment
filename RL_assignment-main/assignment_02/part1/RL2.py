19b5c265a533e07476a269d3c97c2e942b685417
ljh4697 <68049281+ljh4697@users.noreply.github.com>
2021-07-30 14:03:43 +0900

    [docs] RL2 REINFORCE

--- RL_assignment-main/assignment_02/part1/RL2.py
+++ RL_assignment-main/assignment_02/part1/RL2.py
@@ -1,11 +1,81 @@
 import numpy as np
+from tensorflow.python.keras.mixed_precision import policy
 import MDP
 import matplotlib.pyplot as plt
 import math
 import random
 
-class DNN:
-    pass
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.optimizers import Adam
+
+class DNN(tf.keras.Model):
+    def __init__(self, action_size):
+        super(DNN, self).__init__()
+        self.fc1 = Dense(24, activation='relu')
+        self.fc2 = Dense(24, activation='relu')
+        self.fc_out = Dense(action_size, activation='softmax')
+
+    def call(self, input):
+        x = self.fc1(input)
+        x = self.fc2(x)
+        policy = self.fc_out(x)
+        return policy
+
+class REINFORCEAgent:
+    def __init__(self, state_size, action_size):
+        self.state_size = state_size
+        self.action_size = action_size
+
+        #hyper parameter#
+        self.discount_factor = 0.99
+        self.learning_rate = 0.01
+
+        self.model = DNN(self.action_size)
+        self.optimizer = Adam(learning_rate=self.learning_rate)
+        self.states, self.actions, self.rewards = [], [], []
+
+    def get_action(self, state):
+        policy = self.model(state)[0]
+        policy = np.array(policy)
+        cumprob = np.cumsum(policy)
+        action = np.where(cumprob >= np.random.rand(1))[0][0]
+        return action
+
+    def discount_rewards(self, rewards):
+        discounted_rewards = np.zeros_lize(rewards)
+        running_add = 0
+        for t in reversed(range(0, len(rewards))):
+            running_add = running_add * self.discount_factor + rewards[t]
+            discounted_rewards[t] = running_add
+        return discounted_rewards
+
+    def append_sample(self, state, action, reward):
+        self.states.append(state[0])
+        self.rewards.append(reward)
+        act = np.zeros(self.actions_size)
+        act[action] = 1
+        self.actions.append(act)
+
+    def train_model(self):
+        discounted_rewards = np.float32(self.discount_rewards(self.rewards))
+        
+        model_params = self.model.trainable_variables
+        with tf.GradientTape() as tape:
+            tape.watch(model_params)
+            policies = self.model(np.array(self.states))
+            actions = np.array(self.actions)
+            action_prob = tf.reduce_sum(actions * policies, axis=1)
+            cross_entropy = -tf.math.log(action_prob + 1e-5)
+            loss = tf.reduce_sum(cross_entropy * discounted_rewards)
+            entropy =  - policies * tf.math.log(policies)
+
+        grads = tape.gradient(loss, model_params)
+        self.optimizer.apply_gradients(zip(grads, model_params))
+        self.states, self.actions, self.rewards = [], [], []
+        return np.mean(entropy)
+
+
 
 class RL2:
     def __init__(self,mdp,sampleReward):
